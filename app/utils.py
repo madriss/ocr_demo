4e9efb80a476a16b7149fb4aa120dd28f22eb7eb
madriss <m.seksaoui@gmail.com>
2021-08-16 16:49:02 +0200

    Update utils.py with instruction

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 import pytesseract
 import re
-pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract"
+pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract" #  to set to your own tesseract installation folder
 
 # To do utiliser use NER for detecting fields of interest
 
